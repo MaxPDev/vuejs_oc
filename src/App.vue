daae5b51c6623320d22d27e13b3a4231f187f604
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-01-23 12:02:20 +0100

    fc

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import NavLink from './components/NavLink.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      NavLink
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div id="app">
+    <nav>
+      
+      <nav-link url="/" text="Accueil" />
+      <nav-link url="/about" text="About" />
+      <nav-link url="/contact" text="Contact" />
+    </nav>
+  </div>
+  
+</template>
+
+
